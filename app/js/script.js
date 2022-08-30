d866cfe4f3bb2841773a3985601bb46704e3b3a3
lilpardesu <89980151+lilpardesu@users.noreply.github.com>
2022-08-29 22:03:20 -0700

    added one column

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,11 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+
+// detect keypress (letter, backspace, others)
+
+// updste "letters"
+
+//update tiles based on "letters"
+
+// delete last letter (and call update tiles)
