ececa54045a63957097c37c4f11b04fe19e466dc
lilpardesu <89980151+lilpardesu@users.noreply.github.com>
2022-08-30 04:52:19 -0700

    added updateLetters func

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,31 @@
-console.log('HELLO');
-
-const test = () => {
-	console.log('this is a test');
-};
+const letteraPattern = /[A-Za-z]/
+let guessCount = 1;
+let currentGuess = document.querySelector("#guess" + guessCount);
+let currentLetters = currentGuess.dataset.letter;
 
 // detect keypress (letter, backspace, others)
-document.addEventListener('keydown', () => {console.log("Keypress")})
+document.addEventListener('keydown', (e) => {console.log("Keypress: " + e.key)
+
+	//letter
+	let keyPress = e.key;
+
+	if (keyPress.length == 1 && letteraPattern.test(e.key)) {
+	    console.log(isLetter);
+	}
+	 
+
+	//backspace
+
+	//other
+})
 
 // updste "letters"
+const updateLetters = (letter) => {
+	console.log("guessCount: " + guessCount)
+	console.log("currentLetters: " + currentLetters)
+
+	currentLetters = currentLetters + letter
+}
 
 //update tiles based on "letters"
 
