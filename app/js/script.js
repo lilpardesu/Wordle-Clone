ef723dbada3338fac8e7addac2645333e8ecbdde
lilpardesu <89980151+lilpardesu@users.noreply.github.com>
2022-09-02 16:36:29 +0430

    pefect random word selector func added

--- app/js/script.js
+++ app/js/script.js
@@ -2,21 +2,42 @@ const letteraPattern = /[A-Za-z]/
 let guessCount = 1;
 let currentGuess = document.querySelector("#guess" + guessCount);
 let currentLetters = currentGuess.dataset.letters;
+const words = ['apple', 'baker', 'bread', 'hello', 'floor'];
+let solutionWord = "";
 
-// detect keypress (letter, backspace, others)
+// choose a random word from array
+const chooseWord = (array) => {
+	let randomNumber = Math.floor(Math.random() * 10^array.length.toString.length);
+
+	if (randomNumber < array.length){
+		solutionWord = array[randomNumber];
+	}
+	else {
+		chooseWord(words);
+	}
+}
+chooseWord(words);
+console.log(solutionWord)
+
+// detect keypress (letter, backspace, enter, others)
 document.addEventListener('keydown', (e) => {
 
 	let keyPress = e.key
 
 	//letter
-	if (keyPress.length == 1 && letteraPattern.test(e.key)) {
+	if (keyPress.length == 1 && letteraPattern.test(e.key) && currentGuess.dataset.letters.length < 5) {
 		updateLetters(keyPress);
 	}
 	 
 	//backspace
-	else if (keyPress == "Backspace"){
+	else if (keyPress == "Backspace" && currentLetters != null){
 		deleteLetter();
 	}
+
+	//enter
+	// else if (keyPress == "Enter" && == 5) {
+
+	// }
 })
 
 // update "letters"
